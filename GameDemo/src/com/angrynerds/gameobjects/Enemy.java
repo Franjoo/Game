eee9156620e706e1dd4b401c932f3b6a4245c367
osym49 <sebastian-bernard@arcor.de>
2014-01-23 16:44:46 +0100

    Enemy refactored

--- GameDemo/src/com/angrynerds/gameobjects/Enemy.java
+++ GameDemo/src/com/angrynerds/gameobjects/Enemy.java
@@ -60,20 +60,14 @@ public class Enemy extends Creature {
 
     private int speed = 120;
     private int tolerance = 1;
-    private int rea = 0;
+    private int nextStepInPath = 1;
 
     private boolean alive  = true;
-    private boolean flipped = false;
+    private int xTilePosition;
+    private int yTilePosition;
+    private int xTilePlayer;
+    private int yTilePlayer;
 
-
-//    public Enemy(PlayScreen playScreen,Player player){
-//        super();
-//        this.playScreen = playScreen;
-//        map = screena.playController.world.map;
-//        this.player = player;
-//
-//
-//    }
 
     public Enemy(String name, String path, String skin, Player player, float scale) {
         super(name, path, skin, scale);
@@ -105,7 +99,8 @@ public class Enemy extends Creature {
 
 
         ani = skeletonData.findAnimation("move");
-
+        updatePositions();
+        path = getNewPath();
 
 
 
@@ -125,38 +120,68 @@ public class Enemy extends Creature {
     public void update(float deltatime) {
         super.update(deltatime);
 
-        // Orientation to player
 
 
-        //Creates new Path to Player
-        path = pathFinder.findPath(1, (int) (x) / map.getTileWidth(), (int) (y) / map.getTileHeight(), (int) (player.x) / map.getTileWidth(), (int) (player.y) / map.getTileHeight());
         if (alive){
-            skeleton.setFlipX((player.x - x >= 0));
-        if (path != null && rea < path.getLength()) {
+         updatePositions();
+         moveToPlayer(deltatime);
 
-            if (!isReached(rea)) {
+        }
 
-                Vector2 nextStep = new Vector2((float) path.getStep(1).getX() * map.getTileWidth(), (float) path.getStep(1).getY() * map.getTileHeight());
-                float angle = (float) Math.atan2(path.getStep(rea).getY() * map.getTileHeight() - y, path.getStep(rea).getX() * map.getTileWidth() - x);
+        else
+            ani.mix( skeleton, skeleton.getTime(), skeleton.getTime(), false, null,1);
+
+    }
+
+    public void updatePositions(){
+
+        xTilePosition =   (int) (x) / map.getTileWidth();
+        yTilePosition =   (int) (y) / map.getTileHeight();
+        xTilePlayer   =   (int) (player.x) / map.getTileWidth();
+        yTilePlayer   =   (int) (player.y) / map.getTileHeight() ;
+    }
+
+    public Path getNewPath(){
+               //updatePositions();
+        return pathFinder.findPath(1,xTilePosition ,yTilePosition ,xTilePlayer , yTilePlayer);
+    }
+
+    public void moveToPlayer(float deltatime){
+
+        skeleton.setFlipX((player.x - x >= 0));
+
+       // System.out.println(!isReached(nextStepInPath));
+
+        path = getNewPath();
+        if (path != null && nextStepInPath < path.getLength()) {
+
+            if (!isReached(nextStepInPath)) {
+
+                Vector2 nextStep = new Vector2((float) path.getStep(nextStepInPath).getX() * map.getTileWidth(), (float) path.getStep(nextStepInPath).getY() * map.getTileHeight());
+                float angle = (float) Math.atan2(path.getStep(nextStepInPath-1).getY() * map.getTileHeight() - y, path.getStep(nextStepInPath-1).getX() * map.getTileWidth() - x);
                 velocity.set((float) Math.cos(angle) * speed, (float) Math.sin(angle) * speed);
 
 
                 // velocity.set( (nextStep.x - getX()),(nextStep.y - getY()));
 
-                if (x != nextStep.x) {
+                if (xTilePosition != nextStep.x) {
                     x = x + velocity.x * deltatime;
 
                 }
-                if (y != nextStep.y)
+                if (yTilePosition != nextStep.y)
                     y = (y + velocity.y * deltatime);
 
 
-            } else if (isReached(rea)) {
-
-                rea++;
+            }  if (isReached(nextStepInPath)) {
+                System.out.println("Reached");
+                nextStepInPath++;
             }
-            if (rea == path.getLength()) {
+           // System.out.println("player x tile: " + xTilePlayer + ",Player y tile: " + yTilePlayer);
+           // System.out.println("enemy x tile: " + xTilePosition + ",Enemy y tile: " + yTilePosition);
+
+            if (nextStepInPath == path.getLength()) {
                 System.out.println("New Path");
+                path = getNewPath();
 
             }
 
@@ -166,85 +191,10 @@ public class Enemy extends Creature {
                 ani = skeletonData.findAnimation("move");
 
         }
-            ani.apply(skeleton, skeleton.getTime(), skeleton.getTime(), true, null);
-        }
-
-        else
-        ani.mix( skeleton, skeleton.getTime(), skeleton.getTime(), false, null,1);
-       // ani.apply(skeleton, skeleton.getTime(), skeleton.getTime(), false, null);
-
-
-
-
-       // if(getSkeletonBounds().aabbIntersectsSkeleton(player.getSkeletonBounds()))
-       // System.out.println(getSkeletonBounds().getMaxX());
-
-
-
-
-
-        // path = pf.findPath(1,(int)position.x/map.getTileWidth(),(int)position.y/map.getTileHeight(),(int)player.position.x/map.getTileWidth(),(int) player.position.y/map.getTileHeight());
-
-        // for(int i= 0, len =  path.getLength(); i< len;i++){
-        /**
-         int nextDestinationX = path.getStep(i).getX()*map.getTileWidth();
-         int nextDestinationY = path.getStep(i).getY()*map.getTileHeight();
-         while( position.x != nextDestinationX || position.y != nextDestinationY){
-         if(nextDestinationX<position.x)  {
-         position.x -= 1;
-         System.out.println("Walking" + i);                    }
-         else if(nextDestinationX>position.x){
-         position.x += 1;
-
-         }
-         else  if(nextDestinationY<position.y){
-         position.y -= 1 ;
-
-         }
-
-         else   if(nextDestinationY > position.y){
-         position.y +=1;
-
-         }
-         setPosition(position.x,position.y);
-         }  */
+        ani.apply(skeleton, skeleton.getTime(), skeleton.getTime(), true, null);
 
 
     }
-
-
-
-
-       /*
-        if(position.x != pX && position.y != pY){
-          if(pX + player.dimension.x  < position.x ){
-
-            //System.out.println("Going Left");
-            position.x -= 1;
-          }
-           if(pY + player.dimension.y < position.y)     {
-
-             position.y -= 1;
-            //System.out.println("Going Down");
-           }
-
-            if(pX  > position.x + this.dimension.x){
-
-                //System.out.println("Going Ricght");
-                position.x += 1;
-            }
-            if(pY  > position.y + this.dimension.y)     {
-
-                position.y += 1;
-                //  System.out.println("Going Up");
-            }
-        }
-            */
-    //  for(int i = 0; i<path.getLength();i++){
-
-    // setPosition(path.getStep(i).getX(),path.getStep(i).getY());
-    //    System.out.println("Walking");
-    // }
 
     public void hit(int healthDecrease){
          if(player.getState() != State.ATTACKING){
